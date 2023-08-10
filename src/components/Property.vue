03211f9831862de658031cdfa7c623b047545c2b
sjfhsjfh <sjfhsjfh@qq.com>
2023-08-10 17:18:34 +0800

    fix: text overflow

--- src/components/Property.vue
+++ src/components/Property.vue
@@ -8,11 +8,11 @@ withDefaults(defineProps<{ title: string, value: string, multi?: boolean }>(), {
 
 <template>
   <div v-if="!multi" class="flex flex-row">
-    <span class="grow">{{ title }}</span>
-    <span class="ml-4 text-primary-content">{{ value }}</span>
+    <span class="grow whitespace-nowrap">{{ title }}</span>
+    <span class="ml-4 text-primary-content truncate">{{ value }}</span>
   </div>
   <div v-else class="flex flex-col">
-    <span class="text-sm">{{ title }}</span>
-    <span class="text-primary-content">{{ value }}</span>
+    <span class="text-sm whitespace-nowrap">{{ title }}</span>
+    <span class="text-primary-content truncate">{{ value }}</span>
   </div>
 </template>
