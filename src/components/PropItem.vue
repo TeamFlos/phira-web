eab2bea48d0453fd8bd0d19939555a56f94d22c8
mivik <mivik@qq.com>
2023-08-13 13:23:11 +0800

    fix(prop): text color

--- src/components/PropItem.vue
+++ src/components/PropItem.vue
@@ -9,10 +9,10 @@ withDefaults(defineProps<{ title: string, value: string, multi?: boolean }>(), {
 <template>
   <div v-if="!multi" class="flex flex-row">
     <span class="grow whitespace-nowrap">{{ title }}</span>
-    <span class="ml-4 text-primary-content truncate">{{ value }}</span>
+    <span class="ml-4 base-content truncate">{{ value }}</span>
   </div>
   <div v-else class="flex flex-col">
     <span class="text-sm whitespace-nowrap">{{ title }}</span>
-    <span class="text-primary-content truncate">{{ value }}</span>
+    <span class="base-content truncate">{{ value }}</span>
   </div>
 </template>
