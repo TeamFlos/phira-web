636586a9252fd9dcb00075415739efd9e43d03e3
mivik <mivik@qq.com>
2023-08-14 13:23:05 +0800

    tweak(settings): add gap for menu

--- src/SettingsView.vue
+++ src/SettingsView.vue
@@ -17,7 +17,7 @@ const category = computed(() => {
   <div class="flex justify-center mt-8 min-h-[24px] p-6">
     <div class="flex flex-col w-full lg:flex-row lg:w-2/3 gap-4">
       <div>
-        <ul id="category-menu" class="menu bg-base-100 lg:w-56 rounded-box">
+        <ul id="category-menu" class="menu bg-base-100 lg:w-56 rounded-box gap-2">
           <li><router-link to="/settings/account" data-category="account" :class="{ active: category === 'account' }"><i class="fa-solid fa-user"></i>账户</router-link></li>
           <li><router-link to="/settings/security" data-category="security" :class="{ active: category === 'security' }"><i class="fa-solid fa-lock"></i>安全</router-link></li>
           <li><router-link to="/settings/email" data-category="email" :class="{ active: category === 'email' }"><i class="fa-solid fa-envelope"></i>邮件</router-link></li>
