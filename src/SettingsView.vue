d076b6c89db64380bd16c118e34b6a1ed0e68763
mivik <mivik@qq.com>
2023-08-14 20:58:58 +0800

    fix(settings): localization

--- src/SettingsView.vue
+++ src/SettingsView.vue
@@ -1,7 +1,23 @@
+<i18n>
+en:
+  account: Account
+  security: Security
+  email: Email
+
+zh-CN:
+  account: 账户
+  security: 安全
+  email: 邮件
+
+</i18n>
+
 <script setup lang="ts">
 
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
+
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n();
 
 import AccountTab from './settings/AccountTab.vue'
 import SecurityTab from './settings/SecurityTab.vue'
@@ -18,9 +34,9 @@ const category = computed(() => {
     <div class="flex flex-col w-full lg:flex-row lg:w-2/3 gap-4">
       <div>
         <ul id="category-menu" class="menu bg-base-100 lg:w-56 rounded-box gap-2">
-          <li><router-link to="/settings/account" data-category="account" :class="{ active: category === 'account' }"><i class="fa-solid fa-user"></i>账户</router-link></li>
-          <li><router-link to="/settings/security" data-category="security" :class="{ active: category === 'security' }"><i class="fa-solid fa-lock"></i>安全</router-link></li>
-          <li><router-link to="/settings/email" data-category="email" :class="{ active: category === 'email' }"><i class="fa-solid fa-envelope"></i>邮件</router-link></li>
+          <li><router-link to="/settings/account" data-category="account" :class="{ active: category === 'account' }"><i class="fa-solid fa-user"></i>{{ t('account') }}</router-link></li>
+          <li><router-link to="/settings/security" data-category="security" :class="{ active: category === 'security' }"><i class="fa-solid fa-lock"></i>{{ t('security') }}</router-link></li>
+          <li><router-link to="/settings/email" data-category="email" :class="{ active: category === 'email' }"><i class="fa-solid fa-envelope"></i>{{ t('email') }}</router-link></li>
         </ul>
       </div>
       <div class="flex flex-col grow card bg-base-100 min-h-12 p-8">
