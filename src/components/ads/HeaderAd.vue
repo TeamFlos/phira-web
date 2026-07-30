8547748658d66c5cb1894fd34e1553184e01f02f
mivik <mivikq@gmail.com>
2026-07-31 00:45:12 +0800

    Feat(ad): remove test flag

--- src/components/ads/HeaderAd.vue
+++ src/components/ads/HeaderAd.vue
@@ -8,10 +8,8 @@ onMounted(() => {
     console.error('adsbygoogle push failed', e);
   }
 });
-
-const isTest = localStorage.getItem('isTest') === 'true';
 </script>
 
 <template>
-  <ins v-if="isTest" class="adsbygoogle" style="display: block" data-ad-client="ca-pub-5774998175455449" data-ad-slot="2033437821"></ins>
+  <ins class="adsbygoogle" style="display: block" data-ad-client="ca-pub-5774998175455449" data-ad-slot="2033437821"></ins>
 </template>
--- src/components/ads/MultiAd.vue
+++ src/components/ads/MultiAd.vue
@@ -8,9 +8,8 @@ onMounted(() => {
     console.error('adsbygoogle push failed', e);
   }
 });
-const isTest = localStorage.getItem('isTest') === 'true';
 </script>
 
 <template>
-  <ins v-if="isTest" class="adsbygoogle" style="display: block" data-ad-format="autorelaxed" data-ad-client="ca-pub-5774998175455449" data-ad-slot="5487145838"></ins>
+  <ins class="adsbygoogle" style="display: block" data-ad-format="autorelaxed" data-ad-client="ca-pub-5774998175455449" data-ad-slot="5487145838"></ins>
 </template>
