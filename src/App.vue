9d51decf59ad99be35ab9f6cfebd1abb23116a7b
mivik <mivikq@gmail.com>
2026-07-30 18:09:11 +0800

    feat: remove side rail ads

--- src/App.vue
+++ src/App.vue
@@ -16,27 +16,17 @@ export default {};
 </script>
 
 <script setup lang="ts">
-import { useWindowSize } from '@vueuse/core';
-import { computed } from 'vue';
-
 import { toastError } from './common';
 import AdBlockNotice from './components/AdBlockNotice.vue';
 import AppFooter from './components/AppFooter.vue';
 import AppHeader from './components/AppHeader.vue';
 import FooterAd from './components/FooterAd.vue';
 import LoadView from './components/LoadView.vue';
-import SideRailAd from './components/SideRailAd.vue';
 
 onErrorCaptured((err) => {
   toastError(err);
   return false;
 });
-
-// Only mount the side rails when there's genuinely enough spare width beside
-// the content for a 160px ad on each side; otherwise don't render/request
-// them at all (a hidden-but-requested ad can run afoul of AdSense policy).
-const { width } = useWindowSize();
-const showSideRails = computed(() => width.value >= 1600);
 </script>
 
 <template>
@@ -62,8 +52,4 @@ const showSideRails = computed(() => width.value >= 1600);
   </div>
   <AppFooter />
   <AdBlockNotice />
-  <template v-if="showSideRails">
-    <SideRailAd side="left" />
-    <SideRailAd side="right" />
-  </template>
 </template>
